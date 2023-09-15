1763c815eaf331ea6052f2a347ab0e29152af2e6
Harsha Vardhan <harshavardhan.t2020@vitstudent.ac.in>
2023-09-15 15:25:51 +0530

    day 6 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+//things done
+// changedd the login page to statefulwidget
+// put the animated container
+// converted it into circular
+// after pressing using a simple bool
+
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,7 +31,7 @@ class LoginPage extends StatelessWidget {
               height: 20.0,
             ),
       
-            Text("Welcome",style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),),
+            Text("Welcome $name",style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),),
       
             SizedBox(
               height: 20.0,
@@ -34,6 +47,16 @@ class LoginPage extends StatelessWidget {
                     hintText: "Enter UserName",
                     labelText: "UserName",
                   ),
+
+                  //to get the call back
+
+                  onChanged: (value){
+                    name = value;
+                    // only use in stateful
+                    setState(() {
+                      
+                    });
+                  },
                 ),
             
                 TextFormField(
@@ -42,15 +65,67 @@ class LoginPage extends StatelessWidget {
                     hintText: "Enter Password",
                     labelText: "Password",
                   ),
+                  
+
+                  
                 ),
       
                 SizedBox(
                   height: 20.0,
                 ),
+
+                InkWell(
+                  // onTap: (){Navigator.pushNamed(context, MyRoutes.homeRoute);},
+                  onTap: ()async{setState(() {
+                    changeButton = true;
+                  });
+                  
+                  await Future.delayed(Duration(seconds: 1));
+                  Navigator.pushNamed(context,MyRoutes.homeRoute);
+                  
+                  },
+
+                  //after 1 sec it shld to homepage
+                  
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    width: changeButton? 50:150,// for animation otherwise it was normal 6
+
+                    height: 50,
+                    //color: Colors.deepPurple, //if decoration is there so it is present there
+                    alignment: Alignment.center,
+                    child:changeButton
+                    ?Icon(Icons.done,color: Colors.white,)
+                    :Text("Login",
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20
+                    ),),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      // borderRadius: BorderRadius.circular(6)
+                      shape: changeButton
+                      ?BoxShape.circle
+                      :BoxShape.rectangle,
+                    ),
+                  ),
+                )
+
+                // and for this container to make it clickable we have too wrap with inkwell
+
+                // to animate the container
+
+
+
+
+
+
+
       
-                ElevatedButton(onPressed: (){
-                  Navigator.pushNamed(context, MyRoutes.homeRoute);
-                }, child: Text("Login"),style: TextButton.styleFrom(minimumSize: Size(125, 40)))
+                // ElevatedButton(onPressed: (){
+                //   Navigator.pushNamed(context, MyRoutes.homeRoute);
+                // }, child: Text("Login"),style: TextButton.styleFrom(minimumSize: Size(125, 40)))
             
                 ],
               ),
