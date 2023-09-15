e0ad9bc091bfbaa11afd6ba59e261159d119f294
Harsha Vardhan <harshavardhan.t2020@vitstudent.ac.in>
2023-09-15 16:19:43 +0530

    day 8 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,6 +18,22 @@ class _LoginPageState extends State<LoginPage> {
 
   String name = "";
   bool changeButton = false;
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!=null && _formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -40,94 +56,125 @@ class _LoginPageState extends State<LoginPage> {
       
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
-              child: Column(
-                children: [
-                    TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Enter UserName",
-                    labelText: "UserName",
-                  ),
-
-                  //to get the call back
-
-                  onChanged: (value){
-                    name = value;
-                    // only use in stateful
-                    setState(() {
-                      
-                    });
-                  },
-                ),
-            
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "Password",
-                  ),
-                  
-
-                  
-                ),
-      
-                SizedBox(
-                  height: 20.0,
-                ),
-
-                InkWell(
-                  // onTap: (){Navigator.pushNamed(context, MyRoutes.homeRoute);},
-                  onTap: ()async{setState(() {
-                    changeButton = true;
-                  });
-                  
-                  await Future.delayed(Duration(seconds: 1));
-                  Navigator.pushNamed(context,MyRoutes.homeRoute);
-                  
-                  },
-
-                  //after 1 sec it shld to homepage
-                  
-                  child: AnimatedContainer(
-                    duration: Duration(seconds: 1),
-                    width: changeButton? 50:150,// for animation otherwise it was normal 6
-
-                    height: 50,
-                    //color: Colors.deepPurple, //if decoration is there so it is present there
-                    alignment: Alignment.center,
-                    child:changeButton
-                    ?Icon(Icons.done,color: Colors.white,)
-                    :Text("Login",
-                    style: TextStyle(
-                      color:Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20
-                    ),),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      // borderRadius: BorderRadius.circular(6)
-                      shape: changeButton
-                      ?BoxShape.circle
-                      :BoxShape.rectangle,
+              child: Form(
+                key: _formKey, // Add this line to associate the key with the Form
+                child: Column(
+                  children: [
+                      TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter UserName",
+                      labelText: "UserName",
                     ),
+
+                    validator: (value){
+                      if(value!.isEmpty)
+                      {
+                        return "UserName cannot be empty";
+                      }
+                      else{
+                        return null;
+                      }
+
+                      //return null;
+                    },
+              
+                    //to get the call back
+              
+                    onChanged: (value){
+                      name = value;
+                      // only use in stateful
+                      setState(() {
+                        
+                      });
+                    },
                   ),
-                )
+                          
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                    
+                     validator: (value){
+                      if(value!.isEmpty)
+                      {
+                        return "Password cannot be empty";
+                      }
+                      else if(value!.length<6){
+                        return "Password length should be atleast 6";
+                      }
 
-                // and for this container to make it clickable we have too wrap with inkwell
-
-                // to animate the container
-
-
-
-
-
-
-
-      
-                // ElevatedButton(onPressed: (){
-                //   Navigator.pushNamed(context, MyRoutes.homeRoute);
-                // }, child: Text("Login"),style: TextButton.styleFrom(minimumSize: Size(125, 40)))
-            
-                ],
+                      return null;
+                    },
+              
+                    
+                  ),
+                    
+                  SizedBox(
+                    height: 20.0,
+                  ),
+              
+                  Material(
+                    borderRadius: BorderRadius.circular(changeButton?50:10),
+                    color: Colors.deepPurple,
+              
+                    // in child we shld not have decorations
+                    
+                    child: InkWell(
+                      // onTap: (){Navigator.pushNamed(context, MyRoutes.homeRoute);},
+                      onTap: () => moveToHome(context),
+                  
+                      //after 1 sec it shld to homepage
+                      
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        width: changeButton? 50:150,// for animation otherwise it was normal 6
+                  
+                        height: 50,
+                        //color: Colors.deepPurple, //if decoration is there so it is present there
+                        alignment: Alignment.center,
+                        child:changeButton
+                        ?Icon(Icons.done,color: Colors.white,)
+                        :Text("Login",
+                        style: TextStyle(
+                          color:Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                        ),),
+                  
+                  
+                        // in the previous day decoration was there
+                        // now we are giving it under Material ancestor 
+                        // coz we want to see the fill button effect
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          // borderRadius: BorderRadius.circular(6)
+                          shape: changeButton
+                          ?BoxShape.circle
+                          :BoxShape.rectangle,
+                        ),
+                      ),
+                    ),
+                  )
+              
+                  // and for this container to make it clickable we have too wrap with inkwell
+              
+                  // to animate the container
+              
+              
+              
+              
+              
+              
+              
+                    
+                  // ElevatedButton(onPressed: (){
+                  //   Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  // }, child: Text("Login"),style: TextButton.styleFrom(minimumSize: Size(125, 40)))
+                          
+                  ],
+                ),
               ),
             )
       
