92fa6bd4f16b856c3016ff83581236747eebf07d
Harsha Vardhan <harshavardhan.t2020@vitstudent.ac.in>
2023-09-20 11:42:53 +0530

    created materials in flutter

---
+++ lib/models/catalog.dart
@@ -0,0 +1,16 @@
+class Item{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+
+  
+}
+
+final products = [
+   Item("Harsha", "Iphone 12 pro", "this is a phone", 12, "#33505a", "https://img5.gadgetsnow.com/gd/images/products/additional/large/G201754_View_1/mobiles/smartphones/apple-iphone-12-pro-256-gb-pacific-blue-6-gb-ram-.jpg")
+];
